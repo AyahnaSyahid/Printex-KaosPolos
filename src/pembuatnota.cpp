74955614bb162a6af060a109bf6a7f3eb8517b9c
AyahBarudak <ayah.syahid2017@gmail.com>
2025-11-06 07:13:06 +0700

    modified:   src/pembuatnota.cpp

--- src/pembuatnota.cpp
+++ src/pembuatnota.cpp
@@ -1,31 +1,40 @@
 #include "pembuatnota.h"
-#include "ui/ui_pembuatnota.h"
-#include <QStandardItemModel>
+
+#include <qstandarditemmodel.h>
+
 #include <QAction>
 #include <QMenu>
+#include <QStandardItemModel>
+
+#include "ui/ui_pembuatnota.h"
 
 PembuatNota::PembuatNota(QWidget *parent)
-  : ui(new Ui::PembuatNota), sm(new QStandardItemModel(30, 5)), QDialog(parent)
-{
+    : ui(new Ui::PembuatNota),
+      sm(new QStandardItemModel(30, 5)),
+      QDialog(parent) {
   ui->setupUi(this);
   auto menu = new QMenu(this);
   auto sim = menu->addAction("Simpan");
   auto bay = menu->addAction("Bayar");
-  
   ui->pushButton->setMenu(menu);
+
   sm->setHeaderData(0, Qt::Horizontal, "No.");
   sm->setHeaderData(1, Qt::Horizontal, "Produk");
   sm->setHeaderData(2, Qt::Horizontal, "Qty");
   sm->setHeaderData(3, Qt::Horizontal, "Harga");
   sm->setHeaderData(4, Qt::Horizontal, "Total");
   ui->notaTabel->setModel(sm);
-  
-  QStandardItem *item = nullptr;
-  for(int i=0; i<30; ++i) {
-    item  = sm->item(i, 0);
-    item->setEditable(false);
-    item  = sm->item(i, 4);
-    item->setEditable(false);
+
+  QStandardItem *item;
+  for (int i = 0; i < 30; ++i) {
+    QList<QStandardItem *> columns;
+    for (int j = 0; j < 5; ++j) {
+      item = new QStandardItem();
+      if (j == 0 || j == 4) {
+        item->setEditable(false);
+      }
+    }
+    sm->appendRow(columns);
   }
 }
 
