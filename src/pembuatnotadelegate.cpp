1a600f53c57a1d13fa1d6d4f5a3522e238e1c488
A3PLUS <ayah.syahid2017@gmail.com>
2025-11-06 16:16:50 +0700

    masih belum banyak berubah

--- src/pembuatnotadelegate.cpp
+++ src/pembuatnotadelegate.cpp
@@ -3,7 +3,16 @@
 PembuatNotaDelegate::PembuatNotaDelegate(QObject *parent) : QStyledItemDelegate(parent) {}
 PembuatNotaDelegate::~PembuatNotaDelegate();
 
-
-void PembuatNotaDelegate::createEditor(QWidget* parent, const QStyledItemDelegate &opt, const QModelIndex& ix) {
-  
+void PembuatNotaDelegate::createEditor( 
+          QWidget* parent, 
+          const QStyledItemDelegate &opt, 
+          const QModelIndex& ix )
+{
+  switch (ix.column()) {
+    case 1: {
+      
+    }
+    case 2: {}
+    case 3: {}
+  }
 }
